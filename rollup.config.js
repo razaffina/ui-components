7bc098824de2630b00b86479c723fca9cdd6d6c6
Reno Zaffina <Razaffina1@gmail.com>
2021-01-28 21:21:20 -0500

    added tailwind

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,30 @@
-
+import css from 'rollup-plugin-css-only';
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
+import sveltePreprocess from "svelte-preprocess";
 
 const pkg = require('./package.json');
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
-        input: 'src/index.js',
-        output: [
-            { file: pkg.main, 'format': 'umd', name: 'TabLink' }
-        ],
-        plugins: [
-            svelte(),
-            resolve()
-        ],
-};
+  input: 'src/index.js',
+  output: [
+      { file: pkg.main, 'format': 'umd', name: 'TabLink' }
+  ],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess({
+        sourceMap: !production,
+        postcss: {
+          plugins: [
+             require("tailwindcss"),
+             require("autoprefixer"),
+             require("postcss-nesting")
+          ],
+        },
+      }),
+    }),
+    css({ output: 'bundle.css' }),
+    resolve()
+  ]
+}
--- src/components/TabLink.svelte
+++ src/components/TabLink.svelte
@@ -1,1 +1,14 @@
-<h1>TabLink in development</h1>
+<h1 class="bg-blue-50 m-1">TabLink in development</h1>
+
+
+<style global lang="postcss">
+
+  /* only apply purgecss on utilities, per Tailwind docs */
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
+
+  @tailwind utilities;
+
+</style>
